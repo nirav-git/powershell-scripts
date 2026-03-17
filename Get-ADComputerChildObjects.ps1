f4c65f31f57e6fe6213b9f261aab56a497818786
Niravsinh Parmar <117263102+nirav-git@users.noreply.github.com>
2026-03-17 17:28:50 -0400

    Adds script to retrieve AD computer child objects

---
+++ Get-ADComputerChildObjects.ps1
@@ -0,0 +1,20 @@
+# Get-ADComputerChildObjects.ps1
+# Retrieves all child AD objects under a computer account in Active Directory
+
+$computerName = "COMPUTER_NAME"
+$serverName = "domain.controller.fqdn"
+
+Get-ADComputer -Identity $computerName -Server $serverName | ForEach-Object {
+    $childObjects = Get-ADObject -Filter * -SearchBase $_.DistinguishedName -Server $serverName
+
+    $childObjects | ForEach-Object {
+        [PSCustomObject]@{
+            ComputerName      = $_.Parent.Name
+            ChildObjectName   = $_.Name
+            ChildObjectType   = $_.ObjectClass
+            Description       = $_.Description
+            WhenCreated       = $_.WhenCreated
+            DistinguishedName = $_.DistinguishedName
+        }
+    }
+}
